356329d62f5966ef84b465f01d1dd29546b5ac9f
joao horacio do lago <joaolago35@gmail.com>
2022-12-08 13:39:52 -0300

    criaçao das classes de testes da api classe filme e filme categoria

--- src/main/java/br/com/spring/estudo/estudos/repositores/ArmazenamentoRepository.java
+++ src/main/java/br/com/spring/estudo/estudos/repositores/ArmazenamentoRepository.java
@@ -3,7 +3,10 @@ package br.com.spring.estudo.estudos.repositores;
 
 import br.com.spring.estudo.estudos.model.ArmazenamentoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ArmazenamentoRepository extends JpaRepository<ArmazenamentoModel, Integer  > {
+    @Query("SELECT u FROM ArmazenamentoModel u ")
+    public ArmazenamentoModel buscaTodoDadosBase( ArmazenamentoModel armazenamentoModel);
 
 }
